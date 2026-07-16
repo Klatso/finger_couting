3bd7929c04803b7f8f8f0eb32e6e530a9c8cc180
Klatso <klaas.thoele@icloud.com>
2026-07-16 12:53:52 +0200

    added visualization

--- finger_counting.py
+++ finger_counting.py
@@ -4,7 +4,8 @@ import mediapipe as mp
 camera = cv2.VideoCapture(0)
 
 hands = mp.solutions.hands.Hands()
-
+drawing = mp.solutions.drawing_utils
+drawing_style = mp.solutions.drawing_utils.DrawingSpec(color=(52, 172, 76), thickness=1, circle_radius=1)
 finger_points = {
    "Zeigefinger": (6, 8), 
    "Mittelfinger": (10, 12), 
@@ -23,14 +24,18 @@ while True:
     hands_result = hands.process(frame_converted)
 
     if hands_result.multi_hand_landmarks is not None:
+
         for hand in hands_result.multi_hand_landmarks:
             finger_count = 0
-            for finger, (point_joint, point_tip) in finger_points.items():
-                if hand.landmark[point_tip].y < hand.landmark[point_joint].y:
-                    print(f" {finger} ist ausgestreckt")
-                    finger_count += 1
+            drawing.draw_landmarks(frame, hand, mp.solutions.hands.HAND_CONNECTIONS, drawing_style, drawing_style)
 
-    cv2.putText(frame, f"{finger_count} Finger ausgestreckt!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 0, 200), 2)
+            for finger, (point_joint, point_tip) in finger_points.items():
+
+                if hand.landmark[point_tip].y < hand.landmark[point_joint].y:
+                    finger_count += 1
+                    print(f" {finger} ist ausgestreckt")
+                    
+    cv2.putText(frame, f"{finger_count} Finger ausgestreckt!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (52, 172, 76), 2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
